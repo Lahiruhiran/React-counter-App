1a9d5ccf7edda9de91fdc341616b98cde40c7f81
unknown <lahiruhiranwijesooriyansbm@gmail.com>
2020-01-22 15:23:24 +0530

    Counter app using react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import NavBar from './components/navbar';
+import Counters from "./components/counters";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ 
+  render() { 
+    return ( 
+    <React.Fragment>
+    <NavBar totalCounters ={this.state.counters.filter(c => c.value > 0).length}
+    />
+    <main className = "container">
+      <Counters
+      counters ={this.state.counters}
+       onDelete ={this.handleDelete} 
+       onIncrement ={this.handelIncrement}
+       onReset = {this.handelReset}
+      />
+    </main>
+    </React.Fragment>
+    );
+  }
 }
-
+ 
 export default App;
